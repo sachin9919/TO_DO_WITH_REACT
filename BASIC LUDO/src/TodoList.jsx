ed18a9dd92d55f658215a1e56b3b726821aeca15
sam <sachinmishra0080@gmail.com>
2025-07-13 19:56:40 +0530

    added a small feature you can check

--- BASIC LUDO/src/TodoList.jsx
+++ BASIC LUDO/src/TodoList.jsx
@@ -2,7 +2,7 @@ import {useState} from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
-    let [todos , setTodos] = useState([{task: "sample Task" , id: uuidv4()}]);
+    let [todos , setTodos] = useState([{task: "sample Task" , id: uuidv4() , isDone: false}]);
     let [newTodo , setNewTodo] = useState("");
 
     let addNewTask = () => {
@@ -10,19 +10,38 @@ export default function TodoList() {
         // setTodos([...todos , newTodo]);
 
         setTodos((prevTodos) => {
-            return [...prevTodos , {task: newTodo , id: uuidv4()}];
+            return [...prevTodos , {task: newTodo , id: uuidv4() , isDone: false}];
         });
         setNewTodo("");
     };
 
     let updateTodoValue = (event) => {
-        //console.log(event.target.value);
+        //console.log(event.target.value); 
         setNewTodo(event.target.value);
     };
 
     let deleteTodo = (id) => {
         setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
     };
+
+    // USE MAP METHOD TO DO CHANGES INSIDE ARR USING MAP METHOD
+
+    let markAsDone = (id) => {
+        setTodos((prevTodos) => 
+            prevTodos.map((todo) => {
+                if(todo.id == id){
+                    return {
+                        ...todo,
+                        isDone: true,
+                    };
+                }else{
+                    return todo;
+                }
+            })
+        );
+    
+    };
+
 
     return(
         <div>
@@ -38,8 +57,9 @@ export default function TodoList() {
             <ul>
                 {todos.map((todo) => (  // if we want to render something from array in the form of list so we always use map as it is a kind of notion or fixed way to do these things
                     <li key = {todo.id}>
-                        <span>{todo.task}</span>
+                        <span style={todo.isDone? {textDecoration:"line-through"} : {}}>{todo.task}</span>
                         <button onClick={() => deleteTodo(todo.id)}>delete</button>  {/* // ARROOW FUNCTION SIMPLY CREATE COPY OF METHOD DO NOT EXECUTE IT */}
+                        <button onClick= {() => markAsDone(todo.id)}>Mark as done</button>
                     </li>
                 ))}
             </ul>
